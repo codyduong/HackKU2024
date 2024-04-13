4aa139f62b0ed30de5583c3341e699112ae80090
ellatnguyen <ellanguyen37@gmail.com>
2024-04-13 16:47:42 -0500

    home page

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,40 +1,100 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import PageWrapper from '@/components/PageWrapper';
 import { signOut, signIn, useSession } from 'next-auth/react';
 import styled from 'styled-components';
 
+const PageContainer = styled.div`
+  position: relative;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: ${({ loaded }) => (loaded ? '1' : '0')};
+  transition: opacity 1s ease;
+`;
+
+const CenteredContent = styled.div`
+  text-align: center;
+  margin-top: 250px;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 20px;
+  text-align: center;
+`;
+
+const Heading = styled.h1`
+  font-size: 3.5em;
+`;
+
+const StyledButton = styled.button`
+  background-color: #4169e1;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+
+  &:hover {
+    background-color: #3559b3;
+  }
+`;
+
 const MainPage = (): JSX.Element => {
   const { data: session } = useSession();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // Simulate fading in effect after component mounts
+    setTimeout(() => {
+      setLoaded(true);
+    }, 1000); // Adjust the delay as needed
+  }, []);
 
   return (
-    <PageWrapper>
-      <h1> I am currently testing.</h1>
+    <PageContainer loaded={loaded}>
+      <PageWrapper>
+        <CenteredContent>
+          <Heading>Connect, Congregate, Convene.</Heading>
 
-      {session ? (
-        <>
-          <span>Signed in as {session.user?.email}</span>
-          <button
-            onClick={() => {
-              signOut();
-            }}
-          >
-            Sign out
-          </button>
-        </>
-      ) : (
-        <>
-          <span>You are not signed in </span>
-          <button
-            onClick={() => {
-              signIn();
-            }}
-          >
-            Sign in
-          </button>
-        </>
-      )}
-    </PageWrapper>
+          {session ? (
+            <>
+              <span>Signed in as {session.user?.email}</span>
+              <ButtonContainer>
+                <StyledButton
+                  onClick={() => {
+                    signOut();
+                  }}
+                >
+                  Sign out
+                </StyledButton>
+              </ButtonContainer>
+            </>
+          ) : (
+            <>
+              <span>Sign in to Convene now </span>
+              <ButtonContainer>
+                <StyledButton
+                  onClick={() => {
+                    signIn();
+                  }}
+                >
+                  Sign in
+                </StyledButton>
+              </ButtonContainer>
+            </>
+          )}
+        </CenteredContent>
+      </PageWrapper>
+    </PageContainer>
   );
 };
 
